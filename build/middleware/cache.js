6d8c405b3d8f8cb22910033dcbb319db63ded76f
AyyasyRosadi <ayyasyrsd@gmail.com>
2024-03-06 17:56:40 +0800

    setup api-cakepout

--- build/middleware/cache.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cache = void 0;
-const cache = (req, res, next) => {
-    res.set("Cache-Control", "no-store");
-    next();
-};
-exports.cache = cache;
