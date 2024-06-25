0bfea779d7efba39ec5d656eaead5299d8ed9dcc
AyyasyRosadi <ayyasyrsd@gmail.com>
2024-06-25 16:40:48 +0800

    change port

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { Synchronize } from "./config/synchronize"
 import app from "./server"
 
 
-app.listen(9191, async () => {
+app.listen(9933, async () => {
     try {
         await Synchronize()
-        console.log(`Hello iam running on port: 9191`)
+        console.log(`Hello iam running on port: 9933`)
     } catch (err) {
         console.log(err)
     }
